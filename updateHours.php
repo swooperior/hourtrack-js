5e4e31bb9111ed269d8db977d2ac367ba674cdcf
swooperior <swooperior@gmail.com>
2019-12-23 14:18:29 +0000

    Un-commented code after offline persistance checks

--- updateHours.php
+++ updateHours.php
@@ -1,46 +1,46 @@
 <?php
-// error_reporting(0);
+error_reporting(0);
 
-// header('Content-Type: application/json');
+header('Content-Type: application/json');
 
-// $furi = "hours.json";
-// $file = file_get_contents($furi);
-// $records = json_decode($file);
+$furi = "hours.json";
+$file = file_get_contents($furi);
+$records = json_decode($file);
 
 
-// if(!isset($_GET["action"])){
+if(!isset($_GET["action"])){
     
-//     if(isset($_POST["record"])){
-//         $record = $_POST["record"];
-//         save_record($records,$record);
+    if(isset($_POST["record"])){
+        $record = $_POST["record"];
+        save_record($records,$record);
 
 
 
 
-//     }else{
-//         echo(json_encode($records));
-//     }
-// }else{
-//     switch($_GET["action"]){
-//         case("in"):
-//             //Do in action
+    }else{
+        echo(json_encode($records));
+    }
+}else{
+    switch($_GET["action"]){
+        case("in"):
+            //Do in action
             
-//         case("out"):
-//             //Do out action              
-//     }
-// }
+        case("out"):
+            //Do out action              
+    }
+}
 
-// function save_record($records,$record){
+function save_record($records,$record){
     
-//     if(count($records) > 0){
-//         if($records[count($records)-1]->end == ""){
-//             //If the end date of the last record saved was not defined, 
-//             //pop this record from the array so it is not duplicated.
-//            array_pop($records);
-//         }
-//     }
-//     //Push this record to the records array
-//     array_push($records, $record);
-//     //Overwrite the entire contents of the json file with the updated records array.
-//     file_put_contents("hours.json", json_encode($records));
-// }
+    if(count($records) > 0){
+        if($records[count($records)-1]->end == ""){
+            //If the end date of the last record saved was not defined, 
+            //pop this record from the array so it is not duplicated.
+           array_pop($records);
+        }
+    }
+    //Push this record to the records array
+    array_push($records, $record);
+    //Overwrite the entire contents of the json file with the updated records array.
+    file_put_contents("hours.json", json_encode($records));
+}
